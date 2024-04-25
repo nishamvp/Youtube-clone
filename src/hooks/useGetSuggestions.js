bfdb8bbad3f6d0941e4f8fc0079f255ee3a21c90
nishamvp <107843151+nishamvp@users.noreply.github.com>
2024-04-25 23:48:45 +0400

    "Integrate Redux store for caching search suggestions and use it in the useGetSuggestions hook"
    The changes include:
    
    * Importing `useDispatch` and `useSelector` from `react-redux`

--- src/hooks/useGetSuggestions.js
+++ src/hooks/useGetSuggestions.js
@@ -1,28 +1,35 @@
 import { useEffect, useState } from "react";
 import { YOUTUBE_SUGGESTION_API } from "../assets/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { caching } from "../utils/searchSlice";
 
 const useGetSuggestions = (searchQuery) => {
   const [suggestions, setSuggestions] = useState([]);
+  const cache = useSelector((store) => store.search);
+  const dispatch = useDispatch();
+
 
   useEffect(() => {
     let timeout;
-
     const fetchSuggestions = async () => {
       if (searchQuery) {
-        timeout = setTimeout(async () => {
-          const response = await fetch(YOUTUBE_SUGGESTION_API + searchQuery);
-          const json = await response.json();
-          setSuggestions(json[1]);
-        }, 200);
+        const response = await fetch(YOUTUBE_SUGGESTION_API + searchQuery);
+        const json = await response.json();
+        setSuggestions(json[1]);
+        dispatch(caching({ [searchQuery]: json[1] }));
       }
     };
 
-    fetchSuggestions();
+    if (cache[searchQuery]) {
+      setSuggestions(cache[searchQuery]);
+    } else {
+      timeout = setTimeout(fetchSuggestions, 200);
+    }
 
     return () => {
       clearTimeout(timeout);
     };
-  }, [searchQuery]);
+  }, [searchQuery, cache]);
 
   return { suggestions };
 };
---
+++ src/utils/searchSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const searchSlice = createSlice({
+  initialState:{},
+  name: "search",
+  reducers: {
+    caching: (state, action) => {
+      return { ...state, ...action.payload };
+    },
+  },
+}); 
+
+export default searchSlice.reducer;
+export const { caching } = searchSlice.actions;
